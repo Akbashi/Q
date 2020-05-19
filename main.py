c7b3bccb65d668ffd52868cb87be7a8dab4ba1d4
akb <khadembashy@gmail.com>
2020-05-19 19:31:13 +0800

    SQLAlcehmy table doesn't exist

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, render_template, flash, request, url_for, redirect, session
 import queue
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://test:testing123@localhost:3306/MYFIRSTDATABASE'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "thesecretness"
 
 class Case:
     def __init__(self, case_id, case_name, case_type, staff_id,case_status,case_team):
@@ -13,15 +17,60 @@ class Case:
         self.status = case_status
         self.team = case_team
 
-
-
 id = 0
+# ***---*** trying SQLAlchemy
+db = SQLAlchemy(app)
+
+class sers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+# ***---*** trying SQLAlchemy
+@app.route('/register/', methods=['GET','POST'])
+def register():
+    try:
+        if not request.json or not 'username' in request.json:
+            return "Errors 400"
+
+        user = request.json['username']
+        session["user"] = user
+        newser = sers(user , "")
+        db.session.add(newser)
+        db.session.commit()
+
+        return "entry received "
+        # input_id = request.json['id']
+        # return "Success! recieved id: {}".format(input_id)
+
+    except Exception as e:
+        return str(e)
+
+
+@app.route('/homeuser/', methods=['GET', 'POST'])
+def homeuser():
+    try:
+        if "user" in session:
+            user = session["user"]
+            print (user + "from home user page")
+            # return "<div>{{user}}</div>"
+        else:
+            return render_template("home.html")
+    except Exception as e:
+        return str(e)
+
+
+
 
 
 @app.route('/')
 def homepage():
     try:
-        return "<h2>Hello World</h2>"
+        return render_template('home.html')
     except Exception as e:
         return str(e)
 
@@ -66,7 +115,14 @@ def tes_post():
     except Exception as e:
         return str(e)
 
+def caseidcounter():
+    global id
+    return id
+    id = id + 1
+
 
 #
 if __name__ == "__main__":
+    db.create_all
     app.run()
+
